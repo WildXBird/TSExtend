cf9572bca9531880c5c2ac27ed1b695291b1e30f
R6SGROUND <xieqiqiang00@live.com>
2022-06-05 21:03:26 +0800

    ExJSON

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
-import { ExJSON } from "./json"
+// import { ExJSON, JSONable } from "./json"
 
+export { JSONable as JSON } from "./json"
+export { ExJSON as XJSON } from "./json"
 
-export type JSON = ExJSON
+// export type JSON = JSONable
+// export type XJSON = ExJSON
