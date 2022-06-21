2fa0407454384ccea0397e9559a525fbf875c30d
R6SGROUND <xieqiqiang00@live.com>
2022-06-21 20:00:44 +0800

    esm support

--- compiler/build.ts
+++ compiler/build.ts
@@ -1,12 +1,12 @@
 import { buildSync } from "esbuild"
 import fs from "fs"
 import path from "path"
-// const path = require('path');
-// const fs = require('fs');
 const esbuildExternal = ["esbuild"]
 
 
 function emptyDir(path: string) {
+    console.log("empty dir",path)
+
     const files = fs.readdirSync(path);
     files.forEach(file => {
         const filePath = `${path}/${file}`;
@@ -15,29 +15,20 @@ function emptyDir(path: string) {
             emptyDir(filePath);
         } else {
             fs.unlinkSync(filePath);
-            console.log(`删除${file}文件成功`);
         }
     });
 }
 
-/////////////////////////////////////
-// console.log("清空TEMP...")
-const outDir = path.resolve(__dirname, './dist')
+const outDir = path.resolve(__dirname, '../dist')
 if (!fs.existsSync(outDir)) {
     fs.mkdirSync(outDir);
 }
 emptyDir(outDir);
-/////////////////////////////////
-// console.log("创建SecurityOutDir")
-// const SecurityOutDir = path.resolve(TempDir, './SecurityEncoded')
-// if (!fs.existsSync(SecurityOutDir)) {
-//     fs.mkdirSync(SecurityOutDir);
-// }
 
 
-//编译器代码打包
+//CommonJS
 {
-    console.log("编译器代码打包")
+    console.log("building CommonJS")
 
     const result = buildSync({
         entryPoints: [path.resolve(__dirname, '../src/main.ts')],
@@ -47,13 +38,31 @@ emptyDir(outDir);
         target: "node16",
         format: "cjs",
         external: esbuildExternal,
-
     });
 
     if (result.errors.length !== 0) {
         console.log(result)
         throw result
     }
-    console.log(result); // { errors: [], warnings: [] }
 }
-console.log("RunSecurityEncode DONE!")
+//ESM 
+{
+    console.log("building ESM")
+
+    const result = buildSync({
+        entryPoints: [path.resolve(__dirname, '../src/main.ts')],
+        "outfile": path.resolve(__dirname, "../dist/main.mjs"),
+        bundle: true,
+        sourcemap: false,
+        target: "node16",
+        format: "esm",
+        external: esbuildExternal,
+    });
+
+    if (result.errors.length !== 0) {
+        console.log(result)
+        throw result
+    }
+}
+
+console.log("build DONE!")
